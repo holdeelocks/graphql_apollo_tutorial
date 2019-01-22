c9d3bc697344aae74217f59457d1347f60a3febf
Holden B <holden.bucher.92@gmail.com>
2019-01-21 18:18:46 -0600

    more fix attempts

--- server/src/index.js
+++ server/src/index.js
@@ -43,6 +43,6 @@ const server = new ApolloServer({
 const app = express();
 server.applyMiddleware({ app });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+app.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
